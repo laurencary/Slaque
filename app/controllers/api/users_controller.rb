fef1d56667ff370faa82c2cd72983b120442613c
laurencary <armstronglg720@gmail.com>
2023-06-13 08:16:00 -0700

    Add working create in users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,23 @@
 class Api::UsersController < ApplicationController
-  def create
-  end
+	# Since this automatic nesting occurs before the params object ever reaches 
+	# your controllers, if you ever want automatic nesting of attributes that are 
+	# camelCased in the request body, you must include them explicitly, just as 
+	# you did above for password.
+	wrap_parameters include: User.attribute_names + ['password']
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			login!(@user)
+			render json: @user
+		else
+			render json: { errors: @user.errors.full_messages, status: :unprocessable_entity }
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:email, :password)
+	end
 end
