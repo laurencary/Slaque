d317fb21c657bb49fc76f5a3ec76ba7dfd8a22c0
laurencary <armstronglg720@gmail.com>
2023-06-17 14:12:57 -0700

    Add right link on session header

--- frontend/src/components/SessionHeader/index.jsx
+++ frontend/src/components/SessionHeader/index.jsx
@@ -1,15 +1,29 @@
+import { Link } from 'react-router-dom'
 import SlaqueLogo from '../SlaqueLogo'
 import './SessionHeader.css'
 
 const SessionHeader = ({type}) => {
     const title = type === "login" ? "Sign in to Slaque" : "First, enter your credentials"
 
+    const rightHeader = () => {
+        if (type === 'login') {
+            return (
+                <>
+                    <p id='signup-redirect-text'>New to Slaque?</p>
+                    <Link to="/get-started/createnew" id="signup-redirect-link">Create an account</Link>
+                </>
+            )
+        } else {
+            return (<></>)
+        }
+    }
+
     return (
         <>
             <header className="session-header">
                 <div className="left-col"></div>
                 <SlaqueLogo isColorful={true} color="black"/>
-                <div className="right-col"></div>
+                <div className="right-col">{rightHeader()}</div>
             </header>
             <h1 className="session-title">{ title }</h1>
             <p className="suggest">We suggest using the <strong>email address you use at work.</strong></p>
