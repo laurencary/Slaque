fa81a4bda9980261af3c4419e5ed3e4074f44c5d
laurencary <armstronglg720@gmail.com>
2023-06-17 14:24:13 -0700

    Update logo link to welcome if user

--- frontend/src/components/SlaqueLogo/index.js
+++ frontend/src/components/SlaqueLogo/index.js
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import SlackLogo from '../../images/slack_logo.svg'
 import SlackLogoYB from '../../images/slack_logo_yb.svg'
 import './SlaqueLogo.css'
 
 const SlaqueLogo = ({isColorful, color}) => {
+    const user = useSelector(state => state.session.user)
+
+    const home = user ? "/welcome" : "/"
+
     return (
-        <Link to="/"className="logo-container">
+        <Link to={home} className="logo-container">
             <img src={ isColorful ? SlackLogo : SlackLogoYB } className="logo-img" />
             <h1 className="logo-text" style={{color: color}}>slaque</h1>
         </Link>
